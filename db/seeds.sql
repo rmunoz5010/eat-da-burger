b65702ec67cc204d787922f3d56ddeb6d1285107
Ramon Munoz <rgm_munoz@mac.com>
2018-01-23 08:57:49 -0500

    v1.8

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('In and Out Burger', false); 
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Five Guys Burger', false);
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Krusty Krab Burger', false);
